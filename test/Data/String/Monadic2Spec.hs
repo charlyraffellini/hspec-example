8679db45fa0a964d79faa2822f76a44488458715
charlyraffellini <carlosraffellini@gmail.com>
2016-08-18 20:55:01 +0100

    move function inside block

--- test/Data/String/Monadic2Spec.hs
+++ test/Data/String/Monadic2Spec.hs
@@ -3,12 +3,10 @@ module Data.String.Monadic2Spec (spec) where
 import Test.Hspec
 import Parser.Monadic2
 
-simpleParser :: Parser Char
-simpleParser = Parser $ \s -> [(head s, tail s)]
-
 spec :: Spec
 spec = do
   describe "Monadic2" $ do
-    it "string" $ do
+    it "simple parse" $ do
+      let simpleParser = Parser $ \s -> [(head s, tail s)]
       let actual = parse simpleParser "aListOfChar"
       actual `shouldBe` [('a', "ListOfChar")]
