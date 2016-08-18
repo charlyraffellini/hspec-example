ddd8c6b9adf7fe489c3ed23f9ed0236c584e7b6a
charlyraffellini <carlosraffellini@gmail.com>
2016-08-18 20:48:34 +0100

    small improvement

--- test/Data/String/Monadic2Spec.hs
+++ test/Data/String/Monadic2Spec.hs
@@ -3,12 +3,12 @@ module Data.String.Monadic2Spec (spec) where
 import Test.Hspec
 import Parser.Monadic2
 
-p :: Parser Char
-p = Parser $ \s -> [('a',s)]
+simpleParser :: Parser Char
+simpleParser = Parser $ \s -> [(head s, tail s)]
 
 spec :: Spec
 spec = do
   describe "Monadic2" $ do
     it "string" $ do
-      let actual = parse p "aListOfChar"
-      actual `shouldBe` [('a', "aListOfChar")]
+      let actual = parse simpleParser "aListOfChar"
+      actual `shouldBe` [('a', "ListOfChar")]
