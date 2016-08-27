bf3f698f542535d3588b2c50fd317683b9cf74f1
charlyraffellini <carlosraffellini@gmail.com>
2016-08-27 17:03:34 +0100

    proof first monad law

--- test/Data/String/Monadic2Spec.hs
+++ test/Data/String/Monadic2Spec.hs
@@ -7,29 +7,22 @@ import Data.Char
 spec :: Spec
 spec = do
   describe "Monadic2" $ do
-    it "simple parse" $ do
+    it "is just simple parse" $ do
       let simpleParser = Parser $ \s -> [(head s, tail s)]
       let actual = parse simpleParser "aListOfChar"
       actual `shouldBe` [('a', "ListOfChar")]
-    it "continuation" $ do
-      let returnNext c = chr (ord c + 1)
-      let first = Parser $ \s -> [(head s, tail s)]
-      let transform = Parser $ \s -> [(returnNext $ head s, tail s)]
-      let binded = first >>= (\c -> return $ returnNext c)
-      let actual = parse binded "aListOfChar"
-      actual `shouldBe` [('b', "ListOfChar")]
-    it "fmap/2" $ do
+    it "is a functor - fmap/2" $ do
       let aParser = Parser (\s -> [(reverse s,"")])
       let finalParser = fmap (map toUpper) aParser
       let actual = parse finalParser "abc"
       actual `shouldBe` [("CBA", "")]
-    it "first functor law: fmap id = id" $ do
+    it "proof the first functor law: fmap id = id" $ do
       let aParser = Parser (\s -> [(reverse s,"")])
       let fmapIdParser = fmap id aParser
       let actual = parse fmapIdParser "abc"
       let expected = parse aParser "abc"
       actual `shouldBe` expected
-    it "second functor law: fmap (f . g) = fmap f . fmap g" $ do
+    it "proof the second functor law: fmap (f . g) = fmap f . fmap g" $ do
       let upperString = map toUpper
       let aLazyParser = Parser (\s -> [(s,"")])
       let fmapUpperReverse = fmap (upperString . reverse) aLazyParser
@@ -45,3 +38,15 @@ spec = do
       let container = Parser (\s -> [((map toUpper),s)]) <*> Parser (\s -> [(reverse s,"")])
       let actual = runParser container "asd"
       actual `shouldBe` "DSA"
+    it "is monadic - continuation" $ do
+      let returnNext c = chr (ord c + 1)
+      let first = Parser $ \s -> [(head s, tail s)]
+      let transform = Parser $ \s -> [(returnNext $ head s, tail s)]
+      let binded = first >>= (\c -> return $ returnNext c)
+      let actual = parse binded "aListOfChar"
+      actual `shouldBe` [('b', "ListOfChar")]
+    it "proof the first monad law - Left identity - return x >>= f is the same as f x" $ do
+      let computation =  (\x -> Parser (\y -> [([head y] ++ x,tail y)]))
+      let dammThing = return "Stuff" >>= computation
+      let actual = parse dammThing "aString"
+      actual `shouldBe` parse (computation "Stuff") "aString" -- [("aStuff", "String")]
